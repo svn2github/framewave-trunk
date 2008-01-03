9144d43df97da5120dfd0a1ad712ab49ccbcc043
rahulkc <rahulkc@f364fc6c-7e41-0410-b16c-eb8c68fe7df8>
2008-01-03 11:21:24 +0000

    Fixed flags class import; now based on toolset, not operating system.
    git-svn-id: svn://svn.code.sf.net/p/framewave/code/trunk@25 f364fc6c-7e41-0410-b16c-eb8c68fe7df8

--- BuildTools/buildscripts/fwflags.py
+++ BuildTools/buildscripts/fwflags.py
@@ -9,20 +9,15 @@ import os
 import string
 import sys
 
-
-# TODO: Fixup this section to pull flags file correctly
-if sys.platform == 'win32':
-    from fwflags_msvc import *
-elif sys.platform  == 'linux2':
-    from fwflags_gcc import *
-elif sys.platform  == 'sunos5':
-    from fwflags_suncc import *
-elif sys.platform == 'darwin':
-    from fwflags_gcc import *
-
 class fwFlags:
     def __init__ (self, oEnv, lstBuildFactors, sObjectType):
         self.oEnv = oEnv    # no copy, we need to apply the flags to this environment itself
-        if 'msvc'   in lstBuildFactors: self.flags = fwFlags_msvc  (oEnv, lstBuildFactors, sObjectType)
-        if 'gcc'    in lstBuildFactors: self.flags = fwFlags_gcc   (oEnv, lstBuildFactors, sObjectType)
-        if 'suncc'  in lstBuildFactors: self.flags = fwFlags_suncc (oEnv, lstBuildFactors, sObjectType)
+        if 'msvc'   in lstBuildFactors:
+            from fwflags_msvc import fwFlags_msvc
+            self.flags = fwFlags_msvc  (oEnv, lstBuildFactors, sObjectType)
+        if 'gcc'    in lstBuildFactors:
+            from fwflags_gcc import fwFlags_gcc
+            self.flags = fwFlags_gcc   (oEnv, lstBuildFactors, sObjectType)
+        if 'suncc'  in lstBuildFactors:
+            from fwflags_suncc import fwFlags_suncc
+            self.flags = fwFlags_suncc (oEnv, lstBuildFactors, sObjectType)
