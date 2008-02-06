79e3bb75940740699a8154488dc5c99c0a36429b
hdroberson <hdroberson@f364fc6c-7e41-0410-b16c-eb8c68fe7df8>
2008-02-06 20:00:26 +0000

    bnataraj, pass roboedit 20080206
    git-svn-id: svn://svn.code.sf.net/p/framewave/code/trunk@114 f364fc6c-7e41-0410-b16c-eb8c68fe7df8

--- Framewave/doc/fwJPEGIntroDoc.h
+++ Framewave/doc/fwJPEGIntroDoc.h
@@ -84,22 +84,19 @@ FwiDecodeHuffmanState - Internal data structure for Huffman decoder state.
 
 <SectionHeading>JPEG Library</SectionHeading>
 
-<Paragraph>JPEG Library functions perform a variety of tasks related to Joint Photographic Experts Group image manipulation.</Paragraph>
+<Paragraph>JPEG Library<SectDesc> functions perform a variety of tasks related to Joint Photographic Experts Group image manipulation.</SectDesc></Paragraph>
 
 <Paragraph>This section is organized as follows.</Paragraph>
 
-<BulletedList> 
-<Bullet><hlink><uri>need_link</uri><desc>Basic Concepts</desc></hlink> provides an overview of the information contained in the functional descriptions.</Bullet> 
-<Bullet><hlink><uri>need_link</uri><desc>Library Version</desc></hlink> describes the Framewave function that provides library version information.</Bullet> 
-<Bullet><hlink><uri>need_link</uri><desc>Image Compression Functions</desc></hlink> describes the individual functions.</Bullet>
-</BulletedList>
+<SectLinks></SectLinks>
 
 <Paragraph>Within the section, the <Bold>Basic Concepts</Bold> chapter provides overview information related to the functions in the library, 
 and subsequent chapters provide detailed descriptions of library functions that perform operations of the same kind.</Paragraph>
 <Paragraph>Each detailed description consists of a <Bold>Function Name</Bold> followed by a short description, a <Bold>Synopsis</Bold> of the function syntax, 
 a list of function <Bold>Parameters</Bold>, a detailed <Bold>Description</Bold> of the function, and a list of <Bold>Return Values.</Bold></Paragraph> 
 <Paragraph>When a function is optimized for one or more technologies, a list of <Bold>Supported Technologies</Bold> is provided after the short description.</Paragraph>
-<Paragraph>The <hlink><uri>need_link</uri><desc>Function Quick Reference</desc></hlink> provides an index and snapshot view of function optimizations.</Paragraph>
+
+<RefLink></RefLink>
 
 <Chapter>
 <ChapterHeading>Basic Concepts</ChapterHeading>
