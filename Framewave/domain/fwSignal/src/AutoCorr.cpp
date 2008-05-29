20d707607a0b980888274eb3f2d53a8b9fdaf5a7
kiran_s <kiran_s@f364fc6c-7e41-0410-b16c-eb8c68fe7df8>
2008-05-29 13:19:30 +0000

    Summary: Implemented reference code path for Auto-Correlation functions for 32f Data type.
    Reviewer: Ravi Korsa
    
    
    git-svn-id: svn://svn.code.sf.net/p/framewave/code/trunk@334 f364fc6c-7e41-0410-b16c-eb8c68fe7df8

---
+++ Framewave/domain/fwSignal/src/AutoCorr.cpp
@@ -0,0 +1,106 @@
+/*
+Copyright (c) 2006-2008 Advanced Micro Devices, Inc. All Rights Reserved.
+This software is subject to the Apache v2.0 License.
+*/
+
+#include "FwSharedCode.h"
+#include "fwSignal.h"
+
+namespace OPT_LEVEL
+{
+   namespace AutoCorr
+   {
+      template<typename TS>
+      FwStatus AutoCorr_fn(const  TS*  pSrc,  int  srcLen, TS*  pDst,  int  dstLen)
+      {
+         TS sum;
+         
+         for ( int i = 0; i < dstLen; i++ ) {
+            sum = 0;
+            for ( int j = 0; j < srcLen - 1; j++ ) {
+               sum += pSrc[j] * pSrc[j + i];
+            }
+            pDst[i] = sum;
+         }
+         return fwStsNoErr;
+      }
+      
+      template<typename TS>
+      FwStatus AutoCorr_NormA_fn(const  TS*  pSrc,  int  srcLen, TS*  pDst,  int  dstLen)
+      {
+         TS sum;
+         
+         for ( int i = 0; i < dstLen; i++ ) {
+            sum = 0;
+            for ( int j = 0; j < srcLen - 1; j++ ) {
+               sum += pSrc[j] * pSrc[j + i];
+            }
+            pDst[i] = sum/srcLen;                         // Biased
+         }
+         return fwStsNoErr;
+      }
+      
+      template<typename TS>
+      FwStatus AutoCorr_NormB_fn(const  TS*  pSrc,  int  srcLen, TS*  pDst,  int  dstLen)
+      {
+         TS sum;
+         
+         for ( int i = 0; i < dstLen; i++ ) {
+            sum = 0;
+            for ( int j = 0; j < srcLen - 1; j++ ) {
+               sum += pSrc[j] * pSrc[j + i];
+            }
+            pDst[i] = sum/(srcLen - i);                         // Un-Biased
+         }
+         return fwStsNoErr;
+      }
+   }
+}
+
+using namespace OPT_LEVEL;
+
+// Normal Auto-Correlation Functions
+FwStatus PREFIX_OPT(OPT_PREFIX, fwsAutoCorr_32f)(const  Fw32f*  pSrc,  int  srcLen,  Fw32f*  pDst,  int  dstLen)
+{
+   if( FW_REF::PtrNotOK ( pSrc, pDst )) return fwStsNullPtrErr;
+   if( FW_REF::SizeNotOK( srcLen ))     return fwStsLengthErr;
+   if( FW_REF::SizeNotOK( dstLen ))     return fwStsLengthErr;
+     
+   if( dstLen > srcLen)                return fwStsLengthErr;
+   
+   FwStatus result = AutoCorr::AutoCorr_fn<Fw32f>(pSrc, srcLen, pDst, dstLen);
+   return result;
+      
+}
+
+// Biased Auto-Correlation Functions
+FwStatus PREFIX_OPT(OPT_PREFIX, fwsAutoCorr_NormA_32f)(const Fw32f* pSrc, int srcLen, Fw32f* pDst, int dstLen)
+{
+   if( FW_REF::PtrNotOK ( pSrc, pDst )) return fwStsNullPtrErr;
+   if( FW_REF::SizeNotOK( srcLen ))     return fwStsLengthErr;
+   if( FW_REF::SizeNotOK( dstLen ))     return fwStsLengthErr;
+     
+   if( dstLen > srcLen)                return fwStsLengthErr;
+   
+   FwStatus result = AutoCorr::AutoCorr_NormA_fn<Fw32f>(pSrc, srcLen, pDst, dstLen);
+   return result;
+
+}
+
+// Un-Biased Auto-Correlation Functions
+FwStatus PREFIX_OPT(OPT_PREFIX, fwsAutoCorr_NormB_32f)(const Fw32f* pSrc, int srcLen, Fw32f* pDst, int dstLen)
+{
+   if( FW_REF::PtrNotOK ( pSrc, pDst )) return fwStsNullPtrErr;
+   if( FW_REF::SizeNotOK( srcLen ))     return fwStsLengthErr;
+   if( FW_REF::SizeNotOK( dstLen ))     return fwStsLengthErr;
+     
+   if( dstLen > srcLen)                return fwStsLengthErr;
+   
+   FwStatus result = AutoCorr::AutoCorr_NormB_fn<Fw32f>(pSrc, srcLen, pDst, dstLen);
+   return result;
+
+}
+
+
+// Please do NOT remove the above line for CPP files that need to be multipass compiled
+// OREFR
