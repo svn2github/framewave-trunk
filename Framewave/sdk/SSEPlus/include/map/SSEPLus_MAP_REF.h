d2ea2c648345b57a65f75698f54a9ecec0a0b61e
kiran_s <kiran_s@f364fc6c-7e41-0410-b16c-eb8c68fe7df8>
2008-07-08 12:11:14 +0000

    Summary: Added SSEPlus with head revision 72 which is modified for BUILD-Fixes.
    Reviewer: Korsa, Kalyan and Prasad
    
    git-svn-id: svn://svn.code.sf.net/p/framewave/code/trunk@391 f364fc6c-7e41-0410-b16c-eb8c68fe7df8

---
+++ Framewave/sdk/SSEPlus/include/map/SSEPLus_MAP_REF.h
@@ -0,0 +1,28 @@
+//
+// Copyright (c) 2006-2008 Advanced Micro Devices, Inc. All Rights Reserved.
+// This software is subject to the Apache v2.0 License.
+//
+#ifndef __MAP_REF__
+#define __MAP_REF__
+
+//Convert
+#define ssp_convert_3c_3p_epi8 ssp_convert_3c_3p_epi8_REF
+#define ssp_convert_3p_3c_epi8 ssp_convert_3p_3c_epi8_REF
+
+#define ssp_convert_3c_3p_epi16 ssp_convert_3c_3p_epi16_REF
+#define ssp_convert_3p_3c_epi16 ssp_convert_3p_3c_epi16_REF
+
+#define ssp_convert_3c_3p_epi32 ssp_convert_3c_3p_epi32_REF
+#define ssp_convert_3p_3c_epi32 ssp_convert_3p_3c_epi32_REF
+
+#define ssp_convert_4c_4p_epi8 ssp_convert_4c_4p_epi8_REF
+#define ssp_convert_4p_4c_epi8 ssp_convert_4p_4c_epi8_REF
+
+#define ssp_convert_4c_4p_epi16 ssp_convert_4c_4p_epi16_REF
+#define ssp_convert_4p_4c_epi16 ssp_convert_4p_4c_epi16_REF
+
+#define ssp_convert_4c_4p_epi32 ssp_convert_4c_4p_epi32_REF
+#define ssp_convert_4p_4c_epi32 ssp_convert_4p_4c_epi32_REF
+
+#endif //__MAP_REF__
+
